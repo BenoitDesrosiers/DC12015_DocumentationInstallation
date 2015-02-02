9f811a1e83577bbd31180c87c13a30d7c4fe35ad
unknown <desrosib@ST318-01.cegepdrummond.ca>
2015-02-02 11:34:03 -0500

    fix du nom du repertoire

--- entete.php
+++ entete.php
@@ -4,16 +4,16 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Documentation d'installation</title>
-	<link rel="stylesheet" type="text/css" href="/DocumentationInstallation/CSS/Structure.css">
-	<link rel="stylesheet" type="text/css" href="/DocumentationInstallation/CSS/Menu.css">
-	<link rel="stylesheet" type="text/css" href="/DocumentationInstallation/CSS/Button.css">
+	<link rel="stylesheet" type="text/css" href="/DC12015_DocumentationInstallation/CSS/Structure.css">
+	<link rel="stylesheet" type="text/css" href="/DC12015_DocumentationInstallation/CSS/Menu.css">
+	<link rel="stylesheet" type="text/css" href="/DC12015_DocumentationInstallation/CSS/Button.css">
 
 </head>
 <body>
 <div id="conteneur">
 	<div id="principal">
 		<div id="entete">
-			<img id="logoCegep" src="/DocumentationInstallation/Images/logoCegep.png" alt="logoCegep"><br/>
+			<img id="logoCegep" src="/DC12015_DocumentationInstallation/Images/logoCegep.png" alt="logoCegep"><br/>
 		</div> <!-- entete -->
 		
         <div id="enveloppe">
@@ -21,10 +21,10 @@
                 <div class="nav">        
                     <div class="menuNav">
                         <ul class="select">
-                            <li><a href="/DocumentationInstallation/index.php">Acceuil</a></li>
+                            <li><a href="/DC12015_DocumentationInstallation/index.php">Acceuil</a></li>
                         </ul>
                         <ul class="select">
-                            <li><a href="/DocumentationInstallation/FAQ/index.php">FAQ</a></li>
+                            <li><a href="/DC12015_DocumentationInstallation/FAQ/index.php">FAQ</a></li>
                         </ul>
                         <ul class="select">
                             <li><a href="http://www.cdrummond.qc.ca/formations/programmes/reguliers/DEC_technique/informatique.shtm">Département</a></li>
