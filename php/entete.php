dfa10e8f73bb4357f5de064c5f00f6f10bb5ea2e
IMFUZZ <da.junior.du@gmail.com>
2015-03-09 12:00:42 -0400

    Nouvelle architecture des dossiers ainsi qu'un exemple d'une page d'installion (gimp)

---
+++ php/entete.php
@@ -0,0 +1,125 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">	
+	<link rel="stylesheet" href="../ressources/css/entete.css">
+	<link rel="stylesheet" href="../ressources/css/contenu.css">
+	<title><?php echo($titrePage) ?></title>
+</head>
+<body>
+		
+	<nav>
+	    <ul>
+	        <li class='categorie'><p>Bases de données</p>
+	            <div class='menu-categorie'>
+	                <ul>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/Sqlite.png' alt='SQLite'/>
+	                        <h2 class='titre'>SQLite</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/SQLite-studio.png' alt='SQLite Studio'/>
+	                        <h2 class='titre'>SQLite Studio</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/FileZilla.png' alt='Filezilla'/>
+	                        <h2 class='titre'>FileZilla</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                </ul>
+	            </div>
+	        </li>
+	        <li class='categorie'><p>Bureautique</p>
+	            <div class='menu-categorie'>
+	                <ul>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/gimp.png' alt='Gimp'/>
+	                        <h2 class='titre'>Gimp</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/inkscape.png' alt='Inkscape'/>
+	                        <h2 class='titre'>Inkscape</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/audacity.png' alt='Audacity'/>
+	                        <h2 class='titre'>Audacity</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                </ul>
+	            </div>
+	        </li>
+	        <li class='categorie'><p>D&eacute;veloppement</p>
+	            <div class='menu-categorie'>
+	                <ul>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/eiffelstudio.png' alt='EiffelStudio'/>
+	                        <h2 class='titre'>EiffelStudio</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/eclipse.png' alt='Eclipse'/>
+	                        <h2 class='titre'>Eclipse</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/audacity.png' alt='Audacity'/>
+	                        <h2 class='titre'>Audacity</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                </ul>
+	            </div>
+	        </li>
+	        <li class='categorie'><p>Multim&eacute;dia</p>
+	            <div class='menu-categorie'>
+	                <ul>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/gimp.png' alt='Gimp'/>
+	                        <h2 class='titre'>Gimp</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/inkscape.png' alt='Inkscape'/>
+	                        <h2 class='titre'>Inkscape</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/audacity.png' alt='Audacity'/>
+	                        <h2 class='titre'>Audacity</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/AVS.png' alt='Audacity'/>
+	                        <h2 class='titre'>AVS</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                </ul>
+	            </div>
+	        </li>
+	        <li class='categorie'><p>Web</p>
+	            <div class='menu-categorie'>
+	                <ul>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/Chrome.png' alt='Chrome'/>
+	                        <h2 class='titre'>Chrome</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/Firefox.png' alt='Firefox'/>
+	                        <h2 class='titre'>Firefox</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                    <li class='application'>
+	                        <img src='../ressources/images/icones/FileZilla.png' alt='Filezilla'/>
+	                        <h2 class='titre'>FileZilla</h2>
+	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+	                    </li>
+	                </ul>
+	            </div>
+	        </li>
+	    </ul>
+	</nav>
--- tdm.php
+++
@@ -1,9 +0,0 @@
-
-<ul>
-	<li><a href="/DC12015_DocumentationInstallation/index.php">Acceuil</a></li>
-	<li><a href="/DC12015_DocumentationInstallation/texte1/texte1.php">Texte 1</a></li>
-	<ul>
-		<li><a href="/DC12015_DocumentationInstallation/texte1/texte1section1.php">section 1</a></li>
-	</ul>
-	<li><a href="/DC12015_DocumentationInstallation/texte2/texte2.php">Texte 2</a></li>
-</ul>
