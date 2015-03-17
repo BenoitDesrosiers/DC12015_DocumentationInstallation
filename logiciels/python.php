00415fa97ec7d8800bdd47b78e9591657b9b2cf2
IMFUZZ <da.junior.du@gmail.com>
2015-03-17 07:22:35 -0400

    modification de larp.php et python.php ainsi qu'entete.php. ajout de lien fonctionnel

--- logiciels/python.php
+++ logiciels/python.php
@@ -1,11 +1,11 @@
 <?php
-	$titrePage = "Guide d'installation de Gimp";
+	$titrePage = "Guide d'installation de python";
 	include ($_SERVER['DOCUMENT_ROOT'].'/DC12015_DocumentationInstallation/php/entete.php');
 ?>
 
 <div class='contenu'>
 
-<h1>Guide d'installation de GIMP<h1>
+<h1>Guide d'installation de Python<h1>
 
 <div id="table_des_matieres">
     <a href="#presentation"><h2>Présentation</h2></a>
@@ -21,53 +21,53 @@
 </div>
 
 <h2><a name="presentation">Présentation</a></h2>
-    <p>GIMP (acronyme de <i>GNU Image Manipulation Program</i>, aussi orthographié <i>Gimp</i>) est un logiciel de création et d'édition d'images matricielles libre et gratuit.</p>
+    <p>python (acronyme de <i>GNU Image Manipulation Program</i>, aussi orthographié <i>python</i>) est un logiciel de création et d'édition d'images matricielles libre et gratuit.</p>
 
 <h2><a name="windows">Windows</a></h2>
 
 <h3><a name="windows-prealables">Préalables</a></h3>
-    <p>L'installation de GIMP nécessite Windows XP SP3 (ou XP x64 SP2) ou plus récent (Vista, 7 et 8).</p>
+    <p>L'installation de python nécessite Windows XP SP3 (ou XP x64 SP2) ou plus récent (Vista, 7 et 8).</p>
     <p>L'installateur contient les versions 32 et 64-bit ainsi que toutes les dépendances nécessaires.</p>
 
 <h3><a name="windows-telechargement">Téléchargement</a></h3>
-    <p>Des liens de téléchargement peuvent être trouvés sur <a href="www.gimp.org/downloads/">cette page</a>.</p>
-    <p>En date du 27 février 2015, la version la plus récente est la 2.8.14, disponible directement <a href="http://download.gimp.org/pub/gimp/v2.8/windows/gimp-2.8.14-setup.exe">ici</a>. Il est nécessaire d'avoir au moins 87MB d'espace libre sur son disque dur pour pouvoir compléter le téléchargement.</p>
+    <p>Des liens de téléchargement peuvent être trouvés sur <a href="www.python.org/downloads/">cette page</a>.</p>
+    <p>En date du 27 février 2015, la version la plus récente est la 2.8.14, disponible directement <a href="http://download.python.org/pub/python/v2.8/windows/python-2.8.14-setup.exe">ici</a>. Il est nécessaire d'avoir au moins 87MB d'espace libre sur son disque dur pour pouvoir compléter le téléchargement.</p>
 
 <h3><a name="windows-installation">Procédure d'installation</a></h3>
     <p>Lancer l'exécutable. Une fenêtre apparaîtra, offrant de sélectionner langue utilisée par l'installateur. Cependant, le français ne figure pas parmi les options.</p>
-    <img alt="Sélection de la langue" src="../ressources/images/gimp/gimp-1.png"/>
+    <img alt="Sélection de la langue" src="../ressources/images/python/python-1.png"/>
     <p>Une fois la langue sélectionnée, choisir <i>Install</i> pour une installation standard ou <i>Customize</i> pour une installation personnalisée. La suite de ce guide s'applique a une installation personnalisée.</p>
-    <img alt="Installation standard ou personnalisée" src="../ressources/images/gimp/gimp-2.png"/>
+    <img alt="Installation standard ou personnalisée" src="../ressources/images/python/python-2.png"/>
     <p>Prendre connaissance de la license, puis cliquer sur <i>Next</i>.</p>
-    <img alt="Accord de license" src="../ressources/images/gimp/gimp-3.png"/>
+    <img alt="Accord de license" src="../ressources/images/python/python-3.png"/>
     <p>Choisir entre une installation compacte ou complète et choisir les modules individuels, puis cliquer sur <i>Next</i>.</p>
-    <img alt="Installation complète" src="../ressources/images/gimp/gimp-4.png"/>
-    <img alt="Installation minimale" src="../ressources/images/gimp/gimp-5.png"/>
-    <p>Sélectionner les types de fichiers d'image à ouvrir avec GIMP par défaut, puis cliquer sur <i>Next</i>.</p>
-    <img alt="Association de fichiers" src="../ressources/images/gimp/gimp-6.png"/>
+    <img alt="Installation complète" src="../ressources/images/python/python-4.png"/>
+    <img alt="Installation minimale" src="../ressources/images/python/python-5.png"/>
+    <p>Sélectionner les types de fichiers d'image à ouvrir avec python par défaut, puis cliquer sur <i>Next</i>.</p>
+    <img alt="Association de fichiers" src="../ressources/images/python/python-6.png"/>
     <p>Finalement, cocher ou décocher les options pour créer ou non un raccourci sur le bureau et dans la barre des tâches.</p>
-    <img alt="Création de raccourcis" src="../ressources/images/gimp/gimp-7.png"/>
+    <img alt="Création de raccourcis" src="../ressources/images/python/python-7.png"/>
     <p>Réviser les paramètres d'installation. Au besoin, cliquer sur <i>Back</i> pour revenir en arrière et les modifier, puis cliquer sur <i>Install</i> pour procéder à l'installation.</p>
-    <img alt="Révision des paramètres" src="../ressources/images/gimp/gimp-8.png"/>
+    <img alt="Révision des paramètres" src="../ressources/images/python/python-8.png"/>
     <p>À la fin de l'extraction des fichiers, appuyer sur <i>Finish</i> pour terminer le processus.</p>
-    <img alt="Sélection de la langue" src="../ressources/images/gimp/gimp-9.png"/>
-    <img alt="Sélection de la langue" src="../ressources/images/gimp/gimp-10.png"/>
+    <img alt="Sélection de la langue" src="../ressources/images/python/python-9.png"/>
+    <img alt="Sélection de la langue" src="../ressources/images/python/python-10.png"/>
 
 <h2><a name="linux">Linux</a></h2>
 <h3><a name="linux-prealables">Préalables</a></h3>
-    <p>GIMP vient de base avec plusieurs distributions Linux. Dans ce cas, il n'est pas nécessaire de l'installer. Ce n'est pas le cas d'Ubuntu, qui a cessé de l'inclure au printemps 2010.</p>
-    <p>Les packets suivants sont nécessaires au fonctionnement de GIMP:</p>
+    <p>python vient de base avec plusieurs distributions Linux. Dans ce cas, il n'est pas nécessaire de l'installer. Ce n'est pas le cas d'Ubuntu, qui a cessé de l'inclure au printemps 2010.</p>
+    <p>Les packets suivants sont nécessaires au fonctionnement de python:</p>
     <ul>
-        <li>gimp-data</li>
+        <li>python-data</li>
         <li>libbabl</li>
         <li>libgegl</li>
-        <li>libgimp2</li>
+        <li>libpython2</li>
         <li>libilmbase6</li>
         <li>libmng1</li>
         <li>libopenexr6</li>
     </ul>
 <h3><a name="linux-telechargement">Téléchargement</a></h3>
-    <p>La façon propre d'installer GIMP est de passer par le gestionnaire de paquets de sa distribution.</p>
+    <p>La façon propre d'installer python est de passer par le gestionnaire de paquets de sa distribution.</p>
     <p>Dans le cas d'Ubuntu, on peut passer par la Logithèque Ubuntu ou par un terminal.</p>
     <p>Logithèque Ubuntu: ...<p>
     <p>Terminal: ...<p>
@@ -75,8 +75,8 @@
 
 <h2><a name="voir-aussi">Voir aussi:</a></h2>
     <ul>
-        <li><a href="www.gimp.org/">Site officiel de GIMP</a></li>
-        <li><a href="http://doc.ubuntu-fr.org/gimp">GIMP dans la documentation Ubuntu francophone</a></li>
+        <li><a href="www.python.org/">Site officiel de python</a></li>
+        <li><a href="http://doc.ubuntu-fr.org/python">python dans la documentation Ubuntu francophone</a></li>
 
 
 </div>
--- php/entete.php
+++ php/entete.php
@@ -90,16 +90,20 @@
 								<p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
 							</li>
 						</a>
-						<li class='application'>
-	                        <img src='/DC12015_DocumentationInstallation/ressources/images/icones/Larp.png' alt='Larp'/>
-	                        <h2 class='titre'>Larp</h2>
-	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
-	                    </li>
-						<li class='application'>
-	                        <img src='/DC12015_DocumentationInstallation/ressources/images/icones/Python.png' alt='Python'/>
-	                        <h2 class='titre'>Python</h2>
-	                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
-	                    </li>	                    
+						<a href="/DC12015_DocumentationInstallation/logiciels/larp.php">
+							<li class='application'>
+		                        <img src='/DC12015_DocumentationInstallation/ressources/images/icones/Larp.png' alt='Larp'/>
+		                        <h2 class='titre'>Larp</h2>
+		                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+		                    </li>
+						</a>
+						<a href="/DC12015_DocumentationInstallation/logiciels/python.php">
+							<li class='application'>
+		                        <img src='/DC12015_DocumentationInstallation/ressources/images/icones/Python.png' alt='Python'/>
+		                        <h2 class='titre'>Python</h2>
+		                        <p class='description'>Lorem ipsum dolor sit amet, consectetur adipisici elit</p>
+		                    </li>	  
+		                </a>                  
 	                </ul>
 	            </div>
 	        </li>
