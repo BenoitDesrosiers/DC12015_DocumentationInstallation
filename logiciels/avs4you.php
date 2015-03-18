798d79a08218af9b36dd7eb6698e1531d2025f8e
Jeremi Pedneault <jerm95@hotmail.com>
2015-03-17 20:21:50 -0400

    Ma page pour AVS est terminee

--- logiciels/avs4you.php
+++ logiciels/avs4you.php
@@ -21,60 +21,36 @@
 
 <h2><a name="windows">Windows</a></h2>
 
-<h3><a name="windows-prealables">Préalables</a></h3>
-    <p>L'installation de Audacity nécessite Windows 2000 ou plus récent (XP, Vista, 7 et 8).</p>
-    <p>L'installateur contient les versions 32 et 64-bit ainsi que toutes les dépendances nécessaires.</p>
+	<h3><a name="windows-prealables">Préalables</a></h3>
+		<p>L'installation de AVS Video Editor nécessite Windows XP ou plus récent (Vista, 7 et 8).</p>
+		<p>Le logiciel demande minimalement un processeur AMD ou Intel Core 2 Duo de 3GHZ ou plus, 2go de RAM ainsi qu'une carte son compatible windows.</p>
 
-<h3><a name="windows-telechargement">Téléchargement</a></h3>
-    <p>Des liens de téléchargement peuvent être trouvés sur <a href="http://audacity.sourceforge.net/download/">cette page</a>.</p>
-    <p>En date du 16 Mars 2015, la version la plus récente est la 2.0.6, disponible directement <a href="http://sourceforge.net/projects/audacity/files/audacity-win-2.0.6.exe">ici</a>. Il est nécessaire d'avoir au moins 22.9MB d'espace libre sur son disque dur pour pouvoir compléter le téléchargement.</p>
+	<h3><a name="windows-telechargement">Téléchargement</a></h3>
+		<p>Des liens de téléchargement peuvent être trouvés sur <a href="http://www.avs4you.com/fr/AVS-Video-Editor.aspx">cette page</a>.</p>
+		<p>En date du 17 Mars 2015, la version la plus récente est la 7.1.1.259, disponible directement <a href="http://download.avs4you.com/distributives/AVSVideoEditor.exe">ici</a>. Il est nécessaire d'avoir au moins 146.77Mo d'espace libre sur son disque dur pour pouvoir compléter le téléchargement.</p>
 
-<h3><a name="windows-installation">Procédure d'installation</a></h3>
-    <p>Lancer l'exécutable. Une fenêtre apparaîtra, offrant de sélectionner langue utilisée par l'installateur. Sélectionner Français pour la langue.</p>
-    <img alt="Sélection de la langue" src="../ressources/images/audacity/etape_0.png"/>
-    <p>Une fois la langue sélectionnée, choisir <i>Suivant</i> pour continuer l'installation </p>
-    <img alt="Installation standard ou personnalisée" src="../ressources/images/audacity/etape_1.png"/>
-    <p>Prendre connaissance de la license, puis cliquer sur <i>Suivant</i>.</p>
-    <img alt="Accord de license" src="../ressources/images/audacity/etape_2.png"/>
-    <p>Choisir un répertoire pour l'installation à l'aide du bouton <i>parcourir</i>. Laisser par défaut si aucun changement n'est nécessaire, puis cliquer sur <i>Suivant</i>.</p>
-	<img alt="Choix du répertoire" src="../ressources/images/audacity/etape_3.png"/>
-	<p>Sélectionner la case «Create a Desktop icon» si nécessaire, laisser la case <i>Reset Preferences</i> vide, puis cliquer sur <i>Suivant</i>.</p>
-	<img alt="Options d'installation" src="../ressources/images/audacity/etape_4.png"/>
-	<p>Prendre connaissance des informations montrées à l'écran, puis confirmer à l'aide du bouton <i>Suivant</i>.</p>
-	<img alt="Confirmation d'installation" src="../ressources/images/audacity/etape_5.png"/>
-	<p>Patienter pendant l'installation du logiciel.</p>
-	<img alt="Installation du logiciel" src="../ressources/images/audacity/etape_6.png"/>
-	<p>Prendre connaissance des informations de license affichées, puis cliquer sur <i>Suivant</i>.</p>
-	<img alt="Confirmation de license" src="../ressources/images/audacity/etape_7.png"/>
-	<p>L'installation est complétée, sélectionner la case <i>Launch Audacity</i> si nécessaire, puis cliquer sur <i>Terminer</i>.</p>
-	
-	
-
-<h2><a name="linux">Linux</a></h2>
-<h3><a name="linux-prealables">Préalables</a></h3>
-    <p>GIMP vient de base avec plusieurs distributions Linux. Dans ce cas, il n'est pas nécessaire de l'installer. Ce n'est pas le cas d'Ubuntu, qui a cessé de l'inclure au printemps 2010.</p>
-    <p>Les packets suivants sont nécessaires au fonctionnement de GIMP:</p>
-    <ul>
-        <li>gimp-data</li>
-        <li>libbabl</li>
-        <li>libgegl</li>
-        <li>libgimp2</li>
-        <li>libilmbase6</li>
-        <li>libmng1</li>
-        <li>libopenexr6</li>
-    </ul>
-<h3><a name="linux-telechargement">Téléchargement</a></h3>
-    <p>La façon propre d'installer GIMP est de passer par le gestionnaire de paquets de sa distribution.</p>
-    <p>Dans le cas d'Ubuntu, on peut passer par la Logithèque Ubuntu ou par un terminal.</p>
-    <p>Logithèque Ubuntu: ...<p>
-    <p>Terminal: ...<p>
-<h3><a name="linux-installation">Procédure d'installation</a></h3>
+	<h3><a name="windows-installation">Procédure d'installation</a></h3>
+		<p>Lancer l'exécutable. Une fenêtre apparaîtra, offrant de sélectionner langue utilisée par l'installateur. Sélectionner Français pour la langue.</p>
+		<img alt="Sélection de la langue" src="../ressources/images/avs/etape_0.png"/>
+		<p>Une fois la langue sélectionnée, choisir <i>Suivant</i> pour continuer l'installation </p>
+		<img alt="Installation standard ou personnalisée" src="../ressources/images/avs/etape_1.png"/>
+		<p>Prendre connaissance de la license, puis cliquer sur <i>Suivant</i>.</p>
+		<img alt="Accord de license" src="../ressources/images/avs/etape_2.png"/>
+		<p>Choisir un répertoire pour l'installation à l'aide du bouton <i>parcourir</i>. Laisser par défaut si aucun changement n'est nécessaire, puis cliquer sur <i>Suivant</i>.</p>
+		<img alt="Choix du répertoire" src="../ressources/images/avs/etape_3.png"/>
+		<p>Sélectionner la case <i>Créer l'icône du bureau pour AVS Video Editor</i> si nécessaire, laisser la case <i>Installer un échantillon de projet</i> sélectionnée, puis cliquer sur <i>Suivant</i>.</p>
+		<img alt="Options d'installation" src="../ressources/images/avs/etape_4.png"/>
+		<p>Prendre connaissance des informations montrées à l'écran, puis confirmer à l'aide du bouton <i>Suivant</i>.</p>
+		<img alt="Confirmation d'installation" src="../ressources/images/avs/etape_5.png"/>
+		<p>Patienter pendant l'installation du logiciel.</p>
+		<img alt="Installation du logiciel" src="../ressources/images/avs/etape_6.png"/>
+		<p>L'installation est complétée, sélectionner la case <i>Lancer AVS Video Editor</i> si nécessaire, puis cliquer sur <i>Terminer</i>.</p>
+		<img alt="Terminee" src="../ressources/images/avs/etape_7.png"/>
+		
 
 <h2><a name="voir-aussi">Voir aussi:</a></h2>
     <ul>
-        <li><a href="www.gimp.org/">Site officiel de GIMP</a></li>
-        <li><a href="http://doc.ubuntu-fr.org/gimp">GIMP dans la documentation Ubuntu francophone</a></li>
-
+        <li><a href="http://www.avs4you.com/fr/AVS-Audio-Editor.aspx">Site officiel pour AVS Video Editor</a></li>
 
 </div>
 
