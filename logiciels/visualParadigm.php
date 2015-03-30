d4409c2bd36fa942944cc688fe8255731e2fa4cb
Alexandre Girardin <alex_girardin@hotmail.com>
2015-03-30 13:43:58 -0400

    Mise à jour de Visual Paradigm

--- logiciels/visualParadigm.php
+++ logiciels/visualParadigm.php
@@ -17,24 +17,51 @@
 </div>
 
 <h2><a name="presentation">Présentation</a></h2>
-    <p>Visual Paradigm est un modélisateur d'UML visuelle et outil de CAS facile à utiliser, puissant et multi-platform . Visual Paradigm fournit aux développeurs de logiciels la plate-forme de développement d'avant-garde pour construire des applications de qualité plus rapidement, mieux et moins cher!</p>
+    <p>Visual Paradigm est un modélisateur d'UML visuel et outil de CAS facile à utiliser, puissant et multi-platforme . Visual Paradigm fournit aux développeurs de logiciels la plate-forme de développement d'avant-garde pour bâtir des applications de qualité plus rapidement, mieux et pour moins cher!</p>
 
 <h2><a name="windows">Windows</a></h2>
 
 <h3><a name="windows-prealables">Préalables</a></h3>
     <ul>
 		<li>Intel Pentium 4 à 2.0 GHz ou plus récent</li>
-		<li>Minimum 2.0 GB RAM, mais 4.0 GB est recommendé.</li>
+		<li>Minimum 2.0 GB RAM, mais 4.0 GB est recommandé.</li>
 		<li>Minimum 4GB d'espace sur le disque (N'INCLUE PAS la taille des projets).</li>
 		<li>Microsoft Windows (XP/Vista/7/8), Microsoft Windows Server (2000/2003/2008/2012)</li>
 	</ul>
 
 <h3><a name="windows-telechargement">Téléchargement</a></h3>
     <p>Des liens de téléchargement peuvent être trouvés sur <a href="http://www.visual-paradigm.com/download/">cette page</a>.</p>
-    <p>Un version gratuite pour utilisation non commerciale est disponible en 32bit <a href="http://www.visual-paradigm.com/download/community.jsp?platform=windows&arch=64bit">ici</a> et 64bit <a href="http://www.visual-paradigm.com/download/community.jsp?platform=windows&arch=64bit">ici</a>.</p>
+    <p>Un version gratuite pour utilisation non commerciale est disponible en 32bit <a href="http://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_Win32.exe">ici</a> et 64bit <a href="http://www.visual-paradigm.com/downloads/vpce/Visual_Paradigm_CE_Win64.exe">ici</a>.</p>
 	
 <h3><a name="windows-installation">Procédure d'installation</a></h3>
-    <p></p>
+	<img alt="Téléchargement" src="../ressources/images/visualParadigm/telechargement.png"/>
+	<p>Cliquer sur enregistrer et executer l'installateur qui devrait se trouver dans le dossier téléchargement.</p>
+	<img alt="Décompression de l'installateur" src="../ressources/images/visualParadigm/decompression.png"/>
+	<p>Attendre la décompression de l'installateur.</p>
+	<img alt="Bienvenue" src="../ressources/images/visualParadigm/bienvenue.png"/>
+	<p>Cliquer sur <i>Next</i>.</p>
+	<img alt="Accord de liscence" src="../ressources/images/visualParadigm/liscence.png"/>
+	<p>Lire l'accord de liscence et cliquer sur <i>I accept the agreement</i> ensuite sur <i>Next</i>.</p>
+	<img alt="Choix du répertoire" src="../ressources/images/visualParadigm/repertoire.png"/>
+	<p>Sélectionner le répertoire de destination à l'aide du bouton <i>Browse</i>. Une fois le répertoire choisi cliquer sur <i>Next</i>.</p>
+	<img alt="Création du raccourci" src="../ressources/images/visualParadigm/raccourci.png"/>
+	<p>Cocher l'option <i>Create a Start Menu folder</i> si vous souhaiter avoir un dossier dans le menu démarrage. Entrer un nom pour le dossier ou en choisir un existant. Cocher l'option <i>Create shortcuts for all users</i> pour créer un raccourci pour tous les utilisateurs du système. Cliquer sur <i>Next</i>.</p>
+ 	<img alt="Association des fichier .vpp" src="../ressources/images/visualParadigm/association.png"/>
+	<p>Cocher l'option <i>Visual Paradigm Projet (*.vpp)</i> pour associer les fichiers de type "vpp" avec Visual Paradigm. Cliquer sur <i>Next</i>.</p>
+	<img alt="Installation" src="../ressources/images/visualParadigm/installation.png"/>
+	<p>Attendre la fin de l'installation.</p>
+	<img alt="Démarage" src="../ressources/images/visualParadigm/demarage.png"/>
+	<p>Sélectionner l'option <i>Visual Paradigm 12.0</i> si vous voulez démarrer Visual Paradigm suite à la fermeture de l'installateur, sinon sélectionner <i>Don't start</i>. Cliquer sur <i>Finish</i>.</p>
+	<img alt="Installation complèté" src="../ressources/images/visualParadigm/complet.png"/>
+	<p>Cliquer sur <i>Start</i>.</p>
+	<img alt="Inscription" src="../ressources/images/visualParadigm/inscription.png"/>
+	<p>Entrer votre nom dans le champs <i>Name</i> ainsi que votre adresse couriel dans le champs <i>Email address</i> et cliquer sur <i>Get activation code and start Visual Paradigm</i> afin de recevoir votre code d'activation.</p>
+	<img alt="Réception du code d'activation" src="../ressources/images/visualParadigm/code.png"/>
+	<p>Aller chercher votre code d'activation dans votre boître messagerie à l'adresse entrée précédament et cliquer sur <i>Activate</i>.</p>
+	<img alt="Entrée du code d'activation" src="../ressources/images/visualParadigm/codeActivation.png"/>
+	<p>Entrer votre code d'activation ainsi que votre nom. Cliquer sur <i>Activate</i>.</p>
+	<img alt="Activation réussie" src="../ressources/images/visualParadigm/activation.png"/>
+	<p>Cliquer sur <i>OK</i> afin de démarrer Visual Paradigm.</p>
 
 <h2><a name="voir-aussi">Voir aussi:</a></h2>
     <ul>
