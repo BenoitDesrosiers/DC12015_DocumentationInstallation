767fb039e2918505db3163ce74746a27c172ad07
Alexandre Girardin <alex_girardin@hotmail.com>
2015-04-13 10:39:22 -0400

    Ramener version précédante des fichiers index.php et contenu.css

--- index.php
+++ index.php
@@ -3,81 +3,55 @@
 	include ($_SERVER['DOCUMENT_ROOT'].'/DC12015_DocumentationInstallation/php/entete.php');
 ?>
 
-    <div class="index">
-		<h1 class="nom-groupe">Analyse et Conception</h1>
-		<div class="groupe">
-			<a href="/DC12015_DocumentationInstallation/logiciels/caseComplete.php">
-				<div class="tuile">
-					<img alt="Case Complete" src="./ressources/images/icones/Case Complete.png"/>
-					<h2>Case Complete</h2>
-					<p>Logiciel d'analyse graphique</p>
-				</div>
-			</a>
-			<a href="/DC12015_DocumentationInstallation/logiciels/visualParadigm.php">
-				<div class="tuile">
-					<img alt="Visual Paradigm" src="./ressources/images/icones/Visual Paradigm.png"/>
-					<h2>Visual Paradigm</h2>
-					<p>Logiciel de conception et d'analyse graphique</p>
-				</div>
-			</a>
-		</div>
+    <div class="contenu">
+			
+		<h2 class="nomProgrammes" id="programme1"><a href="./pageContenuDemo.php">Programme 1</a></h2>
+			<h3>Description</h3>
+				
+				<p>Cette année encore, nos étudiants finissants se sont trouvé un stage en entreprise. 
+Nos 11 finissants vont faire un stage de 10 semaines dans les entreprises suivantes:
+Centraide, Human-Ware, Leclerc Assurances, N-Média, Siga, SMD, Synaptik Média, et Val Technologie. 
+Durant cette période, ils participeront à l'analyse, la conception, et la programmation de projets informatisés propre à chacune des entreprises hôtes. 
+Ils auront l'occasion de vivre une expérience professionnelle enrichissant sur un plan individuel et social tout en ayant la chance d'acquérir des connaissances s'ajoutant à celles qu'ils ont déjà acquises lors de leur DEC en informatique. 
+Un des buts du stage est de permettre à l'étudiant de compléter sa formation en observant des professionnels du milieu afin de distinguer les intervenants, leurs rôles, leurs responsabilités et de cerner le nivau de communication à adopter avec ses pairs.</p>
+			
+		<h2 class="nomProgrammes" id="programme2"><a href="./pageContenuDemo.php">Programme 2</a></h2>
+			<h3>Description</h3>
+				
+			<p>Cette année encore, nos étudiants finissants se sont trouvé un stage en entreprise. 
+Nos 11 finissants vont faire un stage de 10 semaines dans les entreprises suivantes:
+Centraide, Human-Ware, Leclerc Assurances, N-Média, Siga, SMD, Synaptik Média, et Val Technologie. 
+Durant cette période, ils participeront à l'analyse, la conception, et la programmation de projets informatisés propre à chacune des entreprises hôtes. 
+Ils auront l'occasion de vivre une expérience professionnelle enrichissant sur un plan individuel et social tout en ayant la chance d'acquérir des connaissances s'ajoutant à celles qu'ils ont déjà acquises lors de leur DEC en informatique. 
+Un des buts du stage est de permettre à l'étudiant de compléter sa formation en observant des professionnels du milieu afin de distinguer les intervenants, leurs rôles, leurs responsabilités et de cerner le nivau de communication à adopter avec ses pairs.</p>				
+			
+		<h2 class="nomProgrammes" id="programme3"><a href="./pageContenuDemo.php">Programme 3</a></h2>
+			<h3>Description</h3>
+				
+				<p>Cette année encore, nos étudiants finissants se sont trouvé un stage en entreprise. 
+Nos 11 finissants vont faire un stage de 10 semaines dans les entreprises suivantes:
+Centraide, Human-Ware, Leclerc Assurances, N-Média, Siga, SMD, Synaptik Média, et Val Technologie. 
+Durant cette période, ils participeront à l'analyse, la conception, et la programmation de projets informatisés propre à chacune des entreprises hôtes. 
+Ils auront l'occasion de vivre une expérience professionnelle enrichissant sur un plan individuel et social tout en ayant la chance d'acquérir des connaissances s'ajoutant à celles qu'ils ont déjà acquises lors de leur DEC en informatique. 
+Un des buts du stage est de permettre à l'étudiant de compléter sa formation en observant des professionnels du milieu afin de distinguer les intervenants, leurs rôles, leurs responsabilités et de cerner le nivau de communication à adopter avec ses pairs.</p>	
 		
-		<h1 class="nom-groupe">Bases de données</h1>
-		<div class="groupe">
-			<a href="/DC12015_DocumentationInstallation/logiciels/caseComplete.php">
-				<div class="tuile">
-					<img alt="Case Complete" src="./ressources/images/icones/Case Complete.png"/>
-					<h2>Case Complete</h2>
-					<p>Logiciel d'analyse graphique</p>
-				</div>
-			</a>
-		</div>
+		<h2 class="nomProgrammes"><a href="./logiciels/audacity.php">Audacity</a></h2>
+			<h3>Description</h3>
+				
+				<p>Logiciel libre, gratuit et multi-plateforme permetant de créer et de modifier des fichiers audio. Il propose de nombreuses fonctions permettant d'ajouter des effets sonores ainsi qu'enregistrer ses propres sons. De plus, il permet de convertir ses fichiers audio en plusieurs formats couramment utilisés dans l'industrie. </p>
 		
-		<h1 class="nom-groupe">Bureautique</h1>
-		<div class="groupe">
-			<a href="/DC12015_DocumentationInstallation/logiciels/caseComplete.php">
-				<div class="tuile">
-					<img alt="Case Complete" src="./ressources/images/icones/Case Complete.png"/>
-					<h2>Case Complete</h2>
-					<p>Logiciel d'analyse graphique</p>
-				</div>
-			</a>
-		</div>
+		<h2 class="nomProgrammes"><a href="./logiciels/avs4you.php">Avs4You</a></h2>
+			<h3>Description</h3>
+				
+				<p>Suite de logiciels payante permettant de créer et de modifier des images, des vidéos ainsi que des fichiers audios. Pendant les cours, nous nous cncentrerons sur celui-ci: AvsVideo Editor</p>
+
+		<h2 class="nomProgrammes"><a href="./logiciels/opera_mobile.php">Opera Mobile Emulator</a></h2>
+			<h3>Description</h3>
+				
+				<p>Opera Mobile Emulator est un émulateur de téléphones intelligents et de tablettes qui facilite le développement d'application mobile.</p>
+
+				
 		
-		<h1 class="nom-groupe">Développement</h1>
-		<div class="groupe">
-			<a href="/DC12015_DocumentationInstallation/logiciels/caseComplete.php">
-				<div class="tuile">
-					<img alt="Case Complete" src="./ressources/images/icones/Case Complete.png"/>
-					<h2>Case Complete</h2>
-					<p>Logiciel d'analyse graphique</p>
-				</div>
-			</a>
-		</div>
-		
-		<h1 class="nom-groupe">Multimédia</h1>
-		<div class="groupe">
-			<a href="/DC12015_DocumentationInstallation/logiciels/caseComplete.php">
-				<div class="tuile">
-					<img alt="Case Complete" src="./ressources/images/icones/Case Complete.png"/>
-					<h2>Case Complete</h2>
-					<p>Logiciel d'analyse graphique</p>
-				</div>
-			</a>
-		</div>
-		
-		<h1 class="nom-groupe">Web</h1>
-		<div class="groupe">
-			<a href="/DC12015_DocumentationInstallation/logiciels/caseComplete.php">
-				<div class="tuile">
-					<img alt="Case Complete" src="./ressources/images/icones/Case Complete.png"/>
-					<h2>Case Complete</h2>
-					<p>Logiciel d'analyse graphique</p>
-				</div>
-			</a>
-		</div>
-		
-		</div>
 	</div>
 
 <?php 
